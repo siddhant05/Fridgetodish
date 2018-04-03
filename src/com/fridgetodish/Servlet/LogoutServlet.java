fe953ddd5480f41b8f53ad06a8f48c1afcb575d3
unknown <siddhant05@gmail.com>
2018-04-03 18:53:52 -0500

    Logout page added

---
+++ src/com/fridgetodish/Servlet/LogoutServlet.java
@@ -0,0 +1,43 @@
+package com.fridgetodish.Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+ 
+@WebServlet(value="/LogoutServlet")
+public class LogoutServlet extends HttpServlet {  
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//private static final Logger LOGGER = Logger.getLogger(LogoutServlet.class);
+	//private static final long serialVersionUID = 1L;
+
+		protected void doPost(HttpServletRequest request, HttpServletResponse response)  
+	                    throws ServletException, IOException {
+			
+				response.setContentType("text/html");  
+	            PrintWriter out=response.getWriter();  
+	            request.getRequestDispatcher("/LoginServlet").include(request, response);  
+	            HttpSession session=request.getSession();  
+	            session.invalidate();  
+	            out.close();  
+
+		}  
+		
+		protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+                throws ServletException, IOException{
+			doPost(request, response);
+			
+		}
+		
+		
+	}  
+
