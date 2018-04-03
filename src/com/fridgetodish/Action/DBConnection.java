d47e7dfcdcf4639d8bc14c21175255b3a4277120
unknown <siddhant05@gmail.com>
2018-04-02 20:47:25 -0500

    New connection

--- src/com/fridgetodish/Action/DBConnection.java
+++ src/com/fridgetodish/Action/DBConnection.java
@@ -55,9 +55,15 @@ public class DBConnection{
 
 package com.fridgetodish.Action;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 
 
 
@@ -65,31 +71,45 @@ public class DBConnection{
 	
 	
 	//private static String url = "jdbc:mysql://localhost:3306/fridgetodish?autoReconnect=true&useSSL=false";
-	private static String url=String.format(
+	/*private static String url=String.format(
 		    "jdbc:mysql://google/%s?cloudSqlInstance=%s&"
 			        + "socketFactory=com.google.cloud.sql.mysql.SocketFactory",
 			    "fridgetodish",
 			    "quantum-engine-199903:us-central1:fridge-to-dish");
-   // private static String driverName = "com.mysql.jdbc.Driver";   
+   private static String driverName = "com.mysql.jdbc.Driver";   
     private static String username = "root";   
     private static String password = "mysql1234";
     private static Connection con;
 	//public static final Logger logger = Logger.getLogger(DBConnection.class.toString());
    // private static String urlstring;
+*/    
+    public static Connection getConnection() throws URISyntaxException, SQLException {
+        URI url = new URI(System.getenv("mysql://b753ba6a0e03b3:ad297d2c@us-cdbr-iron-east-05.cleardb.net/heroku_6706d4fbc456e71?reconnect=true"));
 
-    public  Connection getConnection() {
+        String username = url.getUserInfo().split(":")[0];
+        String password = url.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + url.getHost() + url.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+
+   /* public  Connection getConnection() {
     	
            try {
-        	   
+        	 //  Credential creds = GoogleCredential.getApplicationDefault();
         	 con = DriverManager.getConnection(url, username, password);
-			//Class.forName(driverName);
+			Class.forName(driverName);
 		} 
            
            catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-          /*  
+           catch (ClassNotFoundException e) {
+   			// TODO Auto-generated catch block
+   			e.printStackTrace();
+   		}
+            
             try {
                con = DriverManager.getConnection(url, username, password);
             	//con=DriverManager.getConnection(url);
@@ -98,7 +118,7 @@ public class DBConnection{
             } catch (SQLException ex) {
                 // log an exception. fro example:
             	//logger.error("Error Occured While creating connection---->", ex);
-            }*/
+            }
          //catch (ClassNotFoundException ex) {
             // log an exception. for example:
            // System.out.println("Driver not found."); 
@@ -108,5 +128,5 @@ public class DBConnection{
     public static void main (String [] args){
     	new DBConnection().getConnection();
     }
-
+*/
 }
